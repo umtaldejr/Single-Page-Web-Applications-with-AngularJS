5c173e48f99ce3372429decbd24588e016627e61
Exupério Lédo Silva Júnior <elsilva@daitangroup.com>
2017-05-05 22:37:57 -0300

    ShoppingListCheckOffService - create the To Buy List and the Already Bought List - share the lists with the controllers - display the lists in the view

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -9,16 +9,41 @@
 
   function ToBuyController(ShoppingListCheckOffService) {
     var toBuy = this;
+    toBuy.items = ShoppingListCheckOffService.getToBuy();
   };
 
   AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
 
   function AlreadyBoughtController(ShoppingListCheckOffService) {
     var alreadyBought = this;
+    alreadyBought.items = ShoppingListCheckOffService.getAlreadyBought();
   };
 
   function ShoppingListCheckOffService() {
     var service = this;
+    service.getToBuy = function() {
+      return toBuy;
+    };
+    service.getAlreadyBought = function() {
+      return alreadyBought;
+    };
+    var toBuy = [{
+      name: 'apples',
+      quantity: 10
+    }, {
+      name: 'bananas',
+      quantity: 15
+    }, {
+      name: 'oranges',
+      quantity: 5
+    }, {
+      name: 'watermelons',
+      quantity: 2
+    }, {
+      name: 'pears',
+      quantity: 5
+    }];
+    var alreadyBought = [];
   };
 
 })();
