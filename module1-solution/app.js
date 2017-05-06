1f0580c0b02c6431420206b4fa451ff16e0f96c1
Exupério Lédo Silva Júnior <elsilva@daitangroup.com>
2017-05-05 21:58:16 -0300

    Typo - renames the Angular module to LunchCheck

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
 
-  angular.module('MyApp', [])
+  angular.module('LunchCheck', [])
     .controller('LunchCheckController', LunchCheckController);
 
   LunchCheckController.$inject = ['$scope'];
