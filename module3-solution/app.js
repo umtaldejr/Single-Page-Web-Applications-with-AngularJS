5fe3338b9717b9b7fadcbf89f5173de05884d0a3
Exupério Lédo Silva Júnior <elsilva@daitangroup.com>
2017-05-10 18:50:22 -0300

    MenuSearchService - get data through $http - implement the "Narrow It Down" function on the controller - implement the "Remove Item" function on the controller

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -11,14 +11,61 @@
   function NarrowItDownController(MenuSearchService) {
     var ctrl = this;
 
+    ctrl.searchTerm = '';
+    ctrl.foundItems = [];
+
+    ctrl.narrowItDown = narrowItDown;
+    ctrl.removeItem = removeItem;
+
+    function narrowItDown() {
+      var promise = MenuSearchService.getMatchedMenuItems(ctrl.searchTerm);
+      promise.then(
+        function(foundItems) {
+          ctrl.foundItems = foundItems;
+        },
+        function(result) {
+          ctrl.foundItems = [];
+        }
+      );
+    };
+
+    function removeItem(index) {
+      ctrl.foundItems.splice(index, 1);
+    };
+
   };
 
   /* MenuSearchService */
 
-  MenuSearchService.$inject = ['$http'];
+  MenuSearchService.$inject = ['$http', '$q'];
 
-  function MenuSearchService($http) {
+  function MenuSearchService($http, $q) {
     var service = this;
+
+    service.getMatchedMenuItems = function(searchTerm) {
+
+      var promise = $http({
+        method: 'GET',
+        url: 'https://davids-restaurant.herokuapp.com/menu_items.json',
+      });
+
+      var deferred = $q.defer();
+
+      promise.then(
+        // success case
+        function(response) {
+          var foundItems = response.data.menu_items.filter(function(item) {
+            return item.description.search(searchTerm) >= 0;
+          });
+          deferred.resolve(foundItems);
+        },
+        // error case
+        function(response) {
+          deferred.reject();
+        });
+
+      return deferred.promise;
+    };
 
   };
 
