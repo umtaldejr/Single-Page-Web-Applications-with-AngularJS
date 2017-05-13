c67c451c2b6827a895528d48b7b5311d35bd7355
Exupério Lédo Silva Júnior <elsilva@daitangroup.com>
2017-05-13 15:49:20 -0300

    RoutesConfig - state 'home'

--- module4-solution/app/routes.js
+++ module4-solution/app/routes.js
@@ -6,6 +6,17 @@
 
   RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
 
-  function RoutesConfig($stateProvider, $urlRouterProvider) {};
+  function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider
+      .otherwise('/');
+
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'views/home.html'
+      });
+
+  };
 
 })();
