00a59662797ab18cb3192f5b8711a2f480464344
exuperiosilva <elsilva@daitangroup.com>
2017-05-17 10:14:17 -0300

    Module 5 Coding Assignment

---
+++ module5-solution/src/common/common.module.js
@@ -0,0 +1,14 @@
+(function() {
+  "use strict";
+
+  angular.module('common', [])
+    .constant('ApiPath', 'https://ychaikin-course5.herokuapp.com')
+    .config(config);
+
+  config.$inject = ['$httpProvider'];
+
+  function config($httpProvider) {
+    $httpProvider.interceptors.push('loadingHttpInterceptor');
+  }
+
+})();
---
+++ module5-solution/src/public/menu-item/menu-item.component.js
@@ -0,0 +1,21 @@
+(function() {
+  "use strict";
+
+  angular.module('public')
+    .component('menuItem', {
+      templateUrl: 'src/public/menu-item/menu-item.html',
+      bindings: {
+        menuItem: '<'
+      },
+      controller: MenuItemController
+    });
+
+
+  MenuItemController.$inject = ['ApiPath'];
+
+  function MenuItemController(ApiPath) {
+    var $ctrl = this;
+    $ctrl.basePath = ApiPath;
+  }
+
+})();
